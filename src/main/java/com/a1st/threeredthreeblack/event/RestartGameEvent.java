72bbd42caa9feb7ae5c73e23d90a618dc332749b
Abderrahman Youabd <youabd50@gmail.com>
2024-04-21 18:21:27 +0200

    add: new restart functionality added.

---
+++ src/main/java/com/a1st/threeredthreeblack/event/RestartGameEvent.java
@@ -0,0 +1,19 @@
+package com.a1st.threeredthreeblack.event;
+
+import java.util.EventObject;
+
+/**
+ * @author: Abderrahman Youabd aka: A1ST
+ * @version: 1.0
+ */
+public class RestartGameEvent extends EventObject {
+    /**
+     * Constructs a prototypical Event.
+     *
+     * @param source the object on which the Event initially occurred
+     * @throws IllegalArgumentException if source is null
+     */
+    public RestartGameEvent(Object source) {
+        super(source);
+    }
+}
---
+++ src/main/java/com/a1st/threeredthreeblack/event/RestartGameListener.java
@@ -0,0 +1,9 @@
+package com.a1st.threeredthreeblack.event;
+
+/**
+ * @author: Abderrahman Youabd aka: A1ST
+ * @version: 1.0
+ */
+public interface RestartGameListener {
+    void onRestartGame(RestartGameEvent event);
+}
